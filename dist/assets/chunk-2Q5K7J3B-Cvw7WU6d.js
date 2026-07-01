1962b85d9227dcf9fc8e5f156af39e7af454f9c6
fengjutian <fjutian@foxmail.com>
2026-07-01 16:25:50 +0800

    build(dist): 更新构建文件中的依赖引用路径
    更新了多个构建后的JavaScript文件中引用的依赖模块路径，
    包括ExcalidrawBoard、abnfDiagram、ar-SA语言包和arc图表组件，
    以确保正确的模块加载和功能正常运行。

---
+++ dist/assets/chunk-2Q5K7J3B-Cvw7WU6d.js
@@ -0,0 +1,1 @@
+import{_ as i}from"./index-D8zL7ftz.js";var r=class{constructor(t){this.init=t,this.records=this.init()}static{i(this,"ImperativeState")}reset(){this.records=this.init()}};export{r as I};
--- dist/assets/swimlanesDiagram-G3AALYLV-OHwxXoGw.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-23GEKE2U-CYsDyt62.js";import{_ as a}from"./index-CKzpRYik.js";import"./chunk-5VM5RSS4-D9PL7Unn.js";import"./chunk-XXDRQBXY-D4yfjFUj.js";import"./chunk-VR4S4FIN-CVQXkRaH.js";import"./chunk-32BRIVSS-CMH4KEcu.js";import"./channel-BcKgV-oU.js";import"./index-Dyd7TEla.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
